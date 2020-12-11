696da840079b1dede5218b4a7f90a8fee99e7834
Julia-itcourse <juliaartemenko93@gmail.com>
2020-12-11 12:08:18 +0100

    finishing refactoring with toolkit

--- src/redux/contacts/contactsReducer.js
+++ src/redux/contacts/contactsReducer.js
@@ -1,39 +1,66 @@
 import { combineReducers } from "redux"
+import { createReducer } from '@reduxjs/toolkit'
 import contactsActions from './contactsActions'
 
-
-
-const items = (state = [], { type, payload }) => {
-  console.log("contactsReducer.items")
-  switch (type) {
-    case contactsActions.onAddContact.type:
-      console.log(payload.newContact)
-      return [...state, payload.newContact]
-
-    case contactsActions.onRemoveContact.type:
-      return state.filter((contact) => contact.id !== payload)
-
-    default:
-      return state
-  }
+const handleAdd = (state, action) =>{
+    return [...state, action.payload.newContact]
 }
 
-const filter = (state = "", { type, payload }) => {
-  console.log("contactsReducer.filter")
-  switch (type) {
-    case contactsActions.onChangeFilter.type:
-      return payload
-
-    default:
-      return state
-  }
+const handleRemove = (state, action) =>{
+    return state.filter((contact) => contact.id !== action.payload)
 }
+
+const items = createReducer([], {
+    [contactsActions.onAddContact]: handleAdd,
+    [contactsActions.onRemoveContact]: handleRemove,
+})
+
+const filter = createReducer('', {
+    [contactsActions.onChangeFilter]: (state, action) => action.payload
+})
+
 
 export default combineReducers({
   items,
   filter,
 })
 
+//*______________________BEFORE CREATE REDUCER__________________
+// import { combineReducers } from "redux"
+// import contactsActions from './contactsActions'
+
+
+
+// const items = (state = [], { type, payload }) => {
+//   console.log("contactsReducer.items")
+//   switch (type) {
+//     case contactsActions.onAddContact.type:
+//       console.log(payload.newContact)
+//       return [...state, payload.newContact]
+
+//     case contactsActions.onRemoveContact.type:
+//       return state.filter((contact) => contact.id !== payload)
+
+//     default:
+//       return state
+//   }
+// }
+
+// const filter = (state = "", { type, payload }) => {
+//   console.log("contactsReducer.filter")
+//   switch (type) {
+//     case contactsActions.onChangeFilter.type:
+//       return payload
+
+//     default:
+//       return state
+//   }
+// }
+
+// export default combineReducers({
+//   items,
+//   filter,
+// })
 
 // *______________________BEFORE TOOLKIT__________________________
 
